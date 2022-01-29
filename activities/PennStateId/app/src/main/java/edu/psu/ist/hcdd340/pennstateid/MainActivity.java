b65827ec019dd2156455f486e5e2eaba48eebf1e
hcdd-340 <me@saeedabdullah.com>
2022-01-29 17:31:25 -0500

    Adds click event handler template

--- activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/MainActivity.java
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/MainActivity.java
@@ -3,12 +3,25 @@ package edu.psu.ist.hcdd340.pennstateid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "ACTIVITY_4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button next = findViewById(R.id.button_next);
+        next.setOnClickListener(this);
+    }
+    
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, String.format("event source id: %s", view.getId()));
     }
 }
