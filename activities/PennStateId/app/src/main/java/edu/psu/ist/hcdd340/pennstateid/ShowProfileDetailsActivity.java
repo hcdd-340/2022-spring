c2a8128f52ece864ab82e7259e04e2f47699fac3
hcdd-340 <me@saeedabdullah.com>
2022-02-07 18:53:55 -0500

    Starter files for Activity 6

---
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/ShowProfileDetailsActivity.java
@@ -0,0 +1,32 @@
+package edu.psu.ist.hcdd340.pennstateid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ShowProfileDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "ACTIVITY_6_DETAILS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_profile_details);
+
+        findViewById(R.id.button_details_ok).setOnClickListener(this);
+        findViewById(R.id.button_details_cancel).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int eventSourceId = view.getId();
+        Log.d(TAG, String.format("Clicked on: %s", eventSourceId));
+    }
+}
