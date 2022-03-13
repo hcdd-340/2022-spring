40315d3cc7526df4a18fca6874beb67337f37554
hcdd-340 <me@saeedabdullah.com>
2022-03-13 18:18:16 -0400

    Activity 10 starter files

--- activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/ShowProfileDetailsActivity.java
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/ShowProfileDetailsActivity.java
@@ -13,7 +13,7 @@ import org.w3c.dom.Text;
 
 public class ShowProfileDetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG = "ACTIVITY_8_DETAILS";
+    private static final String TAG = "ACTIVITY_10_DETAILS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,5 +83,12 @@ public class ShowProfileDetailsActivity extends AppCompatActivity implements Vie
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "On Destroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        Log.d(TAG, "On Save Instance");
     }
 }
