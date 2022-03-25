76040362467c7cf9540850f5b5a51c013bb96f26
hcdd-340 <me@saeedabdullah.com>
2022-03-25 19:56:27 -0400

    Supports item selection

--- activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/RegisterActivity.java
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/RegisterActivity.java
@@ -5,24 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "ACTIVITY_REGISTER";
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Spinner spinner = findViewById(R.id.spinner_college);
+        spinner = findViewById(R.id.spinner_college);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.penn_state_colleges,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         findViewById(R.id.button_register).setOnClickListener(this);
         findViewById(R.id.button_cancel).setOnClickListener(this);
@@ -34,7 +37,19 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         if (eventSourceId == R.id.button_cancel) {
             finish();
         } else if (eventSourceId == R.id.button_register) {
-            Log.d(TAG, "Register button clicked");
+            String selectedItem = (String) spinner.getSelectedItem();
+            Log.d(TAG, "Register button clicked with: " + selectedItem);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String currentItem = (String) adapterView.getItemAtPosition(i);
+        Log.d(TAG, "Currently selected item: " + currentItem);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d(TAG, "No item selected");
     }
 }
